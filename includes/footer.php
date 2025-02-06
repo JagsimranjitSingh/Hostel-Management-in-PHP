09083d6d6e0eff6054977b01b5c4ac2741a7d708
JagsimranjitSingh <jaskainth38@gmail.com>
2025-02-06 19:25:30 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
         <a style="padding:10px" href="https://www.linkedin.com/in/jagsimranjit-singh-308779225/" target="_blank"><i class="fab fa-linkedin"></i></a>
         <span>|</span>
         <a style="padding:10px" href="https://github.com/Jassagithub" target="_blank"><i class="fab fa-github"></i></a>
-        <a style="padding:10px" href="https://www.linkedin.com/in/jagsimranjit-singh-308779225/" target="_blank"><i class="fab fa-linkedin"></i></a>
+        <a style="padding:10px" href="https://www.linkedin.com/in/jaspreet-undefined-3ab6a034b?trk=contact-info/" target="_blank"><i class="fab fa-linkedin"></i></a>
     </div>
     <hr>
     <div>
